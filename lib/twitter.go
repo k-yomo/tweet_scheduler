25fef4cd246d6a3f09642b9410c619ef83629e8e
k-yomo <kanji.yy@gmail.com>
2019-08-11 17:58:56 +0900

    Make it possble to use placeholder

--- lib/twitter.go
+++ lib/twitter.go
@@ -7,6 +7,7 @@ import (
 	"github.com/labstack/gommon/log"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -31,12 +32,9 @@ func TweetRandomly() {
 	if len(tweets) == 0 {
 		return
 	}
-	if len(tweets) == 1 {
-
-	}
 	rand.Seed(time.Now().UnixNano())
 	tweetIndex := rand.Intn(len(tweets))
-	selectedTweetBody := tweets[tweetIndex].Body
+	selectedTweetBody := replacePlaceholder(tweets[tweetIndex].Body)
 	_, err = api.PostTweet(selectedTweetBody, nil)
 	if err != nil {
 		log.Error(err)
@@ -48,3 +46,13 @@ func TweetRandomly() {
 	}
 }
 
+const (
+	yearMonthPlaceholder = "year_month"
+	monthPlaceholder     = "month"
+)
+
+func replacePlaceholder(body string) string {
+	newBody := strings.ReplaceAll(body, yearMonthPlaceholder, time.Now().Format("2006年1月"))
+	newBody = strings.ReplaceAll(newBody, monthPlaceholder, time.Now().Format("1月"))
+	return newBody
+}
